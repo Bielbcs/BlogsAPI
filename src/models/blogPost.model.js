86e0ec9ef2779592d51d5e521d44eda5722fb42e
Gabriel Bernardo <bielcotrimsv@gmail.com>
2022-10-25 19:50:44 -0300

    req 2

--- src/models/blogPost.model.js
+++ src/models/blogPost.model.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   BlogPost.associate = (models) => {
-    BlogPost.belongsTo(models.user, {
+    BlogPost.belongsTo(models.User, {
       foreignKey: 'user_id',
     });
   }
